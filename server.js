79ac97e9529995ae4260ceee30cd00a9b016f5dd
Nina Withers <ninarosewithers@gmail.com>
2019-02-05 19:19:49 -0500

    adding content to server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Dependencies
 var express = require("express");
 var mongoose = require("mongoose");
-
 var axios = require("axios");
 var cheerio = require("cheerio");
+// morgan & body parser?
+
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -11,11 +12,30 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+// Starting our Express app
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 // Sets up Handlebars
 // =============================================================
-const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Mongoose Database
+// =============================================================
+
+// Schema 
+// =============================================================
+// var db = require("./models")
+
+// Routes
+// =============================================================
+
+// Seeing if Port is listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log('http://localhost' + PORT);
+});
