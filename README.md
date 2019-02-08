4c027e22920511e4b6e0c7e449ab91439f12f81b
Nina Withers <ninarosewithers@gmail.com>
2019-02-07 21:46:45 -0500

    adding changes to articles

--- README.md
+++ README.md
@@ -20,6 +20,6 @@ Check out the heroku deployed app [here]().
 #### Tasks to complete:
 1. ~~Complete connections to Mongoose DB~~
 1. ~~Create overall layout to Frontend~~
-1. Create routes / scraping
+1. Create routes / gscraping
 1. Test/Debug
 1. ~~Heroku deployment~~
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,13 @@
 console.log("hi")
 
-$.get("/getarticles", (data)=>{
-    console.log(data)
+$.getJSON("/getarticles", (data)=>{
+    for (var i = 0; i < 4; i++) {
+        // Display the apropos information on the page
+        $(".card").append("<div class="+"card-body"+">"+"<p data-id='" + data[i]._id + "'>" + "<h2>" +data[i].title + "</h2>" + "<br />"+ data[i].summary +"<br />" + data[i].link + "</p></div> <hr>"
+        // "<button class="btn btn-dark" type="button">" + Save Article + "</button>");
+        )}
 })
+
+{/* <h2>Title</h2>
+        <p>This is some text within a card body.</p>
+        <button class="btn btn-dark" type="button">Save Article</button> */}
